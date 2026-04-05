f0c83257cccb9fb61a02dcaa731f60b4326daf7c
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 22:52:14 +0200

    add atom.mk

---
+++ atom.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+################################################################################
+# gz-common
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gz-physics
+LOCAL_DESCRIPTION := Physics engine abstraction
+
+LOCAL_LIBRARIES := \
+	gz-common \
+	gz-math \
+	gz-plugin \
+	gz-utils \
+	sdformat
+
+LOCAL_CMAKE_CONFIGURE_ARGS := \
+	-DBUILD_TESTING:BOOL=False \
+	-DFREEIMAGE_RUNS=1 \
+	-DFREEIMAGE_RUNS__TRYRUN_OUTPUT=""
+
+# LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/ignition/common3
+
+# LOCAL_EXPORT_LDLIBS := -lignition-common3
+
+include $(BUILD_CMAKE)
+
